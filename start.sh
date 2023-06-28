87a74ef0e7f3bc2f9b44422cb37294702c8e4b5d
Ryan Payne <ryanj.payne@outlook.com>
2023-06-28 11:30:13 -0500

    update start

--- start.sh
+++ start.sh
@@ -39,7 +39,10 @@ env_up(){
 
    aws s3api create-bucket --bucket $S3Bucket --region $AWS_REGION --create-bucket-configuration LocationConstraint=$AWS_REGION
    
-   cd cs-cnap/templates
+   cd /home/ec2-user/cs-cnap/code/
+   zip code -r *
+   cp code.zip /home/ec2-user/cs-cnap/templates/
+   cd /home/ec2-user/cs-cnap/templates
    aws s3 cp . s3://${S3Bucket} --recursive 
    echo -e "$LB\n"
    echo -e "Standing up environment...$NC"
